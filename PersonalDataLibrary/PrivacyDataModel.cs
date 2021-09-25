d0f83326458177bb89eccf7c8903b4e3707a1470
nceng <nceng@uh.edu>
2021-09-25 16:50:45 -0400

    modified documentation and changed class variables

--- PersonalDataLibrary/PrivacyDataModel.cs
+++ PersonalDataLibrary/PrivacyDataModel.cs
@@ -3,15 +3,25 @@
 namespace PersonalDataLibary
 { 
     /// <summary>
-    /// Represents a user's personal request for privacy.
+    /// Represents a user's personal request/approval for private information to be utilized for data analytics and app improvements.
     /// </summary>
     public class PrivacyDataModel
     {
         /// <summary>
-        /// Whether the privacy notice has been requested or not.
+        /// User's approval for data to be shared.
         /// </summary>
-        public bool privacyNotice { get; set; }
-        public String ownerInitials { get; set; }
+        public bool shareData { get; set; }
+        /// <summary>
+        /// User's request for private data stored in database
+        /// </summary>
+        public bool privateDataRequest { get; set; }
+        /// <summary>
+        /// Computer generated timestamp of data request
+        /// </summary>
         public String dateRequestReceived { get; set; }
+        /// <summary>
+        /// User's identification verification for internal approval for data release authorization.
+        /// </summary>
+        public String userDriverLicense { get; set; }
     }
 }
